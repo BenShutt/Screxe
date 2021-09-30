dd2b4458a748f5e980ebefc8ecf196d1b8ee0beb
Ben Shutt <ben@3sidedcube.com>
2021-09-30 16:21:07 +0100

    Create script to download, write, and run tag-commit.sh removing it after.

--- Scripts/tag-commit.sh
+++ Scripts/tag-commit.sh
@@ -1,97 +1,29 @@
-#!/bin/sh
-#
-# Passing in as command line arguments: 
-#   1) A commit SHA hash
-#   2) A version number to tag. e.g. vX.Y.Z
-# 
-# then
-# 1) Tag the given commit with the given version number
-# 2) Merge that tag into master
-# 3) Merge that tag into develop
+#!/bash/sh
 
-# Git directory
-GIT_DIR=".git"
+# Script to download, write, and run ${SCRIPT}. Removing it after.
+# This allows the script to take user input while running.
 
-# Master branch
-MASTER=master
+# Name of script to download and run
+SCRIPT="exe-tag-commit.sh"
 
-# Develop branch
-DEVELOP=develop
+# Remote script
+REMOTE_URL="https://raw.githubusercontent.com/BenShutt/Screxe/master/Scripts/${SCRIPT}"
 
-# Abort script if a command fails
-set -e
-
-# Get the name of the script
-SCRIPT_NAME=`basename "$0"`
-
-# Fail with an error
-fail() {
-    echo "$1" 1>&2
-    exit 1
+# Prepare clean up
+function cleanup {
+    echo "Cleaning..."
+    rm -rf ${SCRIPT}
 }
 
-# Usage description if invalid arguments provided
-usage() {
-    fail "USAGE: ${SCRIPT_NAME} <commit> <version>" 
-}
+# Clean up on exit
+trap cleanup EXIT
 
-# Check command line argument count
-if [ "$#" -ne 2 ]; then
-    usage
-fi
+# Clean and recreate
+cleanup
 
-# Get commit hash from first command line argument
-commit="$1"
+# Fetch and write remote script
+curl -s ${REMOTE_URL} -o "${SCRIPT}"
 
-# Get version number from second command line argument
-version="$2"
-
-# Check for Git repository
-if [ ! -d "${GIT_DIR}" ]; then
-    fail "Not a Git repository"
-fi
-  
-# Print Git repository found
-echo "Found Git repository: $(pwd)"
-
-# Fetch without making changes
-git fetch --dry-run
-
-# Check commit exists
-if ! git cat-file -e ${commit} 2> /dev/null ; then
-  fail "Failed to find commit: ${commit}"
-fi
-
-# Check if tag already exists
-if [ $(git tag -l "${version}") ]; then
-    fail "Tag already exists: ${version}"
-fi
-
-# Prompt the user to confirm
-read -r -p "Please confirm you would like to tag commit '${commit}' with version '${version}'? [y/N] " response
-if [[ !("$response" =~ ^([yY][eE][sS]|[yY])$) ]]; then
-    fail "Aborting..."
-fi
-
-# Tag the commit
-git tag -a ${version} ${commit} -m "Release ${version}"
-
-# Push the tag to the remote
-git push --tags
-
-# Prompt user to confirm they want to merge into master
-read -r -p "Would you like to merge ${version} into ${MASTER}? [y/N] " response
-if [[ ("$response" =~ ^([yY][eE][sS]|[yY])$) ]]; then
-    git checkout ${MASTER}
-    git merge ${version}
-    git push
-fi
-
-# Prompt user to confirm they want to merge into develop
-read -r -p "Would you like to merge ${version} into ${DEVELOP}? [y/N] " response
-if [[ ("$response" =~ ^([yY][eE][sS]|[yY])$) ]]; then
-    git checkout ${DEVELOP}
-    git merge ${version}
-    git push
-fi
+# Execute remote script
+sh ${SCRIPT} $@
 
