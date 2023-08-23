86600ca6a2d0cd3eca942944a913e6b4d9419b08
Ben Shutt <ben@3sidedcube.com>
2023-08-23 08:18:29 +0100

    Update removeAndroidStudio.sh

--- Scripts/removeAndroidStudio.sh
+++ Scripts/removeAndroidStudio.sh
@@ -9,30 +9,30 @@
 
 # Deletes the Android Studio application
 # Note that this may be different depending on what you named the application as, or whether you downloaded the preview version
-rm -Rf /Applications/Android\ Studio.app
+rm -rf /Applications/Android\ Studio.app
 
 # Delete All Android Studio related preferences
 # The asterisk here should target all folders/files beginning with the string before it
-rm -Rf ~/Library/Preferences/AndroidStudio*
-rm -Rf ~/Library/Preferences/Google/AndroidStudio*
+rm -rf ~/Library/Preferences/AndroidStudio*
+rm -rf ~/Library/Preferences/Google/AndroidStudio*
 
 # Deletes the Android Studio's plist file
-rm -Rf ~/Library/Preferences/com.google.android.*
+rm -rf ~/Library/Preferences/com.google.android.*
 
 # Deletes the Android Emulator's plist file
-rm -Rf ~/Library/Preferences/com.android.*
+rm -rf ~/Library/Preferences/com.android.*
 
 # Deletes mainly plugins (or at least according to what mine (Edric) contains)
-rm -Rf ~/Library/Application\ Support/AndroidStudio*
-rm -Rf ~/Library/Application\ Support/Google/AndroidStudio*
+rm -rf ~/Library/Application\ Support/AndroidStudio*
+rm -rf ~/Library/Application\ Support/Google/AndroidStudio*
 
 # Deletes all logs that Android Studio outputs
-rm -Rf ~/Library/Logs/AndroidStudio*
-rm -Rf ~/Library/Logs/Google/AndroidStudio*
+rm -rf ~/Library/Logs/AndroidStudio*
+rm -rf ~/Library/Logs/Google/AndroidStudio*
 
 # Deletes Android Studio's caches
-rm -Rf ~/Library/Caches/AndroidStudio*
+rm -rf ~/Library/Caches/AndroidStudio*
 
 # Deletes older versions of Android Studio
-rm -Rf ~/.AndroidStudio*
+rm -rf ~/.AndroidStudio*
 
