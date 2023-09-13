29b98503b125126c6e775844045c066ec0c9b127
Ben Shutt <shuttbenjamin@gmail.com>
2023-09-13 16:35:24 +0100

    Update remove-xcode.sh

--- Scripts/remove-xcode.sh
+++ Scripts/remove-xcode.sh
@@ -18,10 +18,10 @@ if [[ !("$response" =~ ^([yY][eE][sS]|[yY])$) ]]; then
 fi
 
 # Delete files
-rm -rf /Applications/Xcode.app
-rm -rf /Library/Preferences/com.apple.dt.Xcode.plist
-rm -rf ~/Library/Preferences/com.apple.dt.Xcode.plist
-rm -rf ~/Library/Caches/com.apple.dt.Xcode
-rm -rf ~/Library/Application Support/Xcode
-rm -rf ~/Library/Developer/Xcode
-rm -rf ~/Library/Developer/CoreSimulator
+rm -rf "/Applications/Xcode.app"
+rm -rf "/Library/Preferences/com.apple.dt.Xcode.plist"
+rm -rf "~/Library/Preferences/com.apple.dt.Xcode.plist"
+rm -rf "~/Library/Caches/com.apple.dt.Xcode"
+rm -rf "~/Library/Application Support/Xcode"
+rm -rf "~/Library/Developer/Xcode"
+rm -rf "~/Library/Developer/CoreSimulator"
