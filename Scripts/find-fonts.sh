be298e3d1ab10efe1ba612112217389ee2262350
Ben Shutt <ben.c.shutt@gmail.com>
2024-07-22 20:27:21 +0100

    Update find-fonts.sh

--- Scripts/find-fonts.sh
+++ Scripts/find-fonts.sh
@@ -5,32 +5,32 @@
 # Usage: ./find-fonts.sh <path/to/directory>
 #
 # Prints out the (.ttf) font files in the given directory
-# that may mapped to, say, a Swift enum or the Info.plist.
+# as a list and as an XML for the Info.plist.
 #
 
 # Set defaults
 set -o nounset -o errexit -o errtrace -o pipefail
 
-# Check the command line argument is provided
+# Check the command-line argument is provided
 if [ "$#" -ne 1 ]; then
-    echo "Please provide a directory as a command line argument" 1>&2
+    echo "Please provide a directory as a command-line argument" 1>&2
     exit 1
 fi
 
-# Check the command line argument is a directory 
+# Check the command-line argument is a directory 
 if [ ! -d "$1" ]; then
     echo "$1 is not a valid directory" 1>&2
     exit 1
 fi
 
-# Array of font files
+# Get array of font files
 files=( $(find "$1" -maxdepth 1 -type f -name "*.ttf" -exec basename {} \;) )
 
-# Print the font files
+# Print the font files in a list
 echo "==================== LIST ===================="
 printf '%s\n' "${files[@]}"
 
-# Print the XML for the Info.plist
+# Print the font files as an XML for the Info.plist
 echo "==================== XML ===================="
 echo '<?xml version="1.0" encoding="UTF-8"?>'
 echo '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">'
